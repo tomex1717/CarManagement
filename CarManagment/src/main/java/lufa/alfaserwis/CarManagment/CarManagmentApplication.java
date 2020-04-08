2edd8eca51e29aa42518efa53b35316cdceb1781
tomex1717 <tomex1717@gmail.com>
2020-04-08 17:56:51 +0200

    First Commit

---
+++ CarManagment/src/main/java/lufa/alfaserwis/CarManagment/CarManagmentApplication.java
@@ -0,0 +1,15 @@
+package lufa.alfaserwis.CarManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class CarManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManagmentApplication.class, args);
+		System.out.println("gównho");
+	}
+
+}
---
+++ CarManagment/src/main/java/lufa/alfaserwis/CarManagment/ServletInitializer.java
@@ -0,0 +1,13 @@
+package lufa.alfaserwis.CarManagment;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CarManagmentApplication.class);
+	}
+
+}
