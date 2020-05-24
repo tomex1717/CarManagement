24e5309b29c04e9ebe1bdea172778674194b5a65
tomex1717 <tomex1717@gmail.com>
2020-05-24 18:47:12 +0200

    Added remember me - token in cookies.

--- CarManagment/src/main/java/lufa/alfaserwis/CarManagment/config/DataSourceConfig.java
+++ CarManagment/src/main/java/lufa/alfaserwis/CarManagment/config/DataSourceConfig.java
@@ -1,6 +1,5 @@
 package lufa.alfaserwis.CarManagment.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +8,7 @@ import org.springframework.context.annotation.Primary;
 import javax.sql.DataSource;
 
 @Configuration
+
 public class DataSourceConfig {
 
 
@@ -16,19 +16,18 @@ public class DataSourceConfig {
     @Primary
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url("jdbc:mysql://localhost:3306/car_management?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC");
+        dataSourceBuilder.url("jdbc:mysql://127.0.0.1:3306/car_management?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC");
         dataSourceBuilder.username("hbstudent");
         dataSourceBuilder.password("hbstudent");
         return dataSourceBuilder.build();
     }
 
     @Bean(name = "usersDataSource")
-    @ConfigurationProperties("user.datasource")
-    public DataSource dataSource2(){
+    public DataSource dataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url("jdbc:mysql://users:3306/car_management?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC");
+        dataSourceBuilder.url("jdbc:mysql://127.0.0.1:3306/users?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC");
         dataSourceBuilder.username("hbstudent");
         dataSourceBuilder.password("hbstudent");
-        return DataSourceBuilder.create().build();
+        return dataSourceBuilder.build();
     }
 }
