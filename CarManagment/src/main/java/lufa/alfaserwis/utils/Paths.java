b3f8e28ad44e2326a2caa8c6380eb9701ab53019
tomex1717 <tomex1717@gmail.com>
2020-06-03 16:53:55 +0200

    Added acceess denied 403 error page.

--- CarManagment/src/main/java/lufa/alfaserwis/utils/Paths.java
+++ CarManagment/src/main/java/lufa/alfaserwis/utils/Paths.java
@@ -4,8 +4,10 @@ public final class Paths {
     public static final String PHOTO_PATH = "C://CarManagementPics//";
     public static final String INVOICES_PATH = "C://CarManagementInvoices//";
 
-//    /usr/local/tomcat/webapps/images/
+//    public static final String PHOTO_PATH = "/usr/local/tomcat/webapps/images/";
+//    public static final String INVOICES_PATH = "/usr/local/tomcat/webapps/invoices/";
 
-//    public static final String PHOTO_PATH = "C://CarManagementPics//";
-//    public static final String INVOICES_PATH = "C://CarManagementInvoices//";
+
+
+
 }
