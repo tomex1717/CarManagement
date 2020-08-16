73c5fbdc7071b742154086936862a2076396cf8f
tomex1717 <tomex1717@gmail.com>
2020-08-16 16:34:05 +0200

    TCP Client connections are now counted and logged.

--- CarManagment/src/main/java/lufa/alfaserwis/CarManagment/service/ServerTCPSocketClientHandler.java
+++ CarManagment/src/main/java/lufa/alfaserwis/CarManagment/service/ServerTCPSocketClientHandler.java
@@ -96,6 +96,7 @@ public class ServerTCPSocketClientHandler {
 
             } catch (IOException e) {
                 e.printStackTrace();
+                numberOfConnectedClients--;
 
             }
         }
