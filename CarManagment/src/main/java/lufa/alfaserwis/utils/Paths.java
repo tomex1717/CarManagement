627303106a4b7f7c5a16025d44e4cc327ce89db5
tomex1717 <tomex1717@gmail.com>
2020-08-16 22:29:31 +0200

    Changed day list into dropdown menu in showmap view.

--- CarManagment/src/main/java/lufa/alfaserwis/utils/Paths.java
+++ CarManagment/src/main/java/lufa/alfaserwis/utils/Paths.java
@@ -1,13 +1,13 @@
 package lufa.alfaserwis.utils;
 
 public final class Paths {
-//    public static final String PHOTO_PATH = "C://CarManagementPics//";
-//    public static final String INVOICES_PATH = "C://CarManagementInvoices//";
-//    public static final String REPORT_FILE_PATH = "C://CarManagementPics//reports.txt";
-//    public static final String LOGGING_FILE_PATH = "C://CarManagementPics//log.txt";
+    public static final String PHOTO_PATH = "C://CarManagementPics//";
+    public static final String INVOICES_PATH = "C://CarManagementInvoices//";
+    public static final String REPORT_FILE_PATH = "C://CarManagementPics//reports.txt";
+    public static final String LOGGING_FILE_PATH = "C://CarManagementPics//log.txt";
 
-    public static final String PHOTO_PATH = "/usr/local/tomcat/webapps/images/";
-    public static final String INVOICES_PATH = "/usr/local/tomcat/webapps/invoices/";
-    public static final String REPORT_FILE_PATH = "/usr/local/tomcat/webapps/images/file.txt";
-    public static final String LOGGING_FILE_PATH = "/usr/local/tomcat/webapps/images/log.txt";
+//    public static final String PHOTO_PATH = "/usr/local/tomcat/webapps/images/";
+//    public static final String INVOICES_PATH = "/usr/local/tomcat/webapps/invoices/";
+//    public static final String REPORT_FILE_PATH = "/usr/local/tomcat/webapps/images/file.txt";
+//    public static final String LOGGING_FILE_PATH = "/usr/local/tomcat/webapps/images/log.txt";
 }
