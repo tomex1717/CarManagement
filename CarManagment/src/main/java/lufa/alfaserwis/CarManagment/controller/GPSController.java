51d9e576738cb04027109a04662a7a2fdfd4df4f
tomex1717 <tomex1717@gmail.com>
2020-10-15 14:37:12 +0200

    Changed bad mappings in CarManagement controller (showmap mapping)

--- CarManagment/src/main/java/lufa/alfaserwis/CarManagment/controller/GPSController.java
+++ CarManagment/src/main/java/lufa/alfaserwis/CarManagment/controller/GPSController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @PropertySource("classpath:application.properties")
@@ -29,20 +28,7 @@ public class GPSController {
         this.reportService = reportService;
     }
 
-    @GetMapping("/showmaplatest")
-    public String showMap(Model model, @RequestParam("regNumber") String regNumber) {
 
-
-        model.addAttribute("regnumber", regNumber);
-        List<Report> reportList = reportService.getReportsOfDay(LocalDateTime.now().getNano(), regNumber);
-        model.addAttribute("apikey", googleMapsAPIKey);
-
-        model.addAttribute("reportArray",
-                reportService.makeDirectionsAsJsonArray(reportList));
-
-
-        return "show-map";
-    }
 
     @GetMapping("/showmap")
     public String showMap(Model model, @RequestParam("regNumber") String regNumber, @RequestParam("timestamp") long timestamp) {
